1ee832f9d9f637905d03f8b5a922cca43771d54d
bitm <1759743261@qq.com>
2026-04-09 08:51:18 +0800

    新增性别枚举

---
+++ ETool.Core/Enum/Gender.cs
@@ -0,0 +1,18 @@
+﻿namespace ETool.Core.Enum
+{
+    /// <summary>
+    /// 性别枚举
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// 男性
+        /// </summary>
+        Male,
+
+        /// <summary>
+        /// 女性
+        /// </summary>
+        Female
+    }
+}
